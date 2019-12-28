175c6dc9bc8e81e61f0f355377f103103e9f3002
scd7896 <scd1212@naver.com>
2019-12-28 20:51:50 +0900

    디비 연결

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>라라벨 입문 </title>
+    </head>
+    <body>
+        <div>
+            상단 네브바
+        </div>
+        @yield('content')
+        <div>
+            하단 푸터
+        </div>
+    </body>
+</html>
